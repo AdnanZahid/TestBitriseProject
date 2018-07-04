2709260739e244a16caaec9722d800c9982cc48e
AdnanZahid <adonztevez@ymail.com>
2018-07-04 13:42:23 +0400

    Created dummy repo and added a unit test

--- TestBitriseProjectTests/TestBitriseProjectTests.swift
+++ TestBitriseProjectTests/TestBitriseProjectTests.swift
@@ -11,26 +11,10 @@ import XCTest
 
 class TestBitriseProjectTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testAddingTwoNumbers() {
+        let number1 = 1
+        let number2 = 2
+        let sum = number1 + number2
+        XCTAssertEqual(sum, 3)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
